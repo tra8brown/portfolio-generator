cf931fcc9eda148063ed4d9a99410f859face416
TraBee <tra8brown@gmail.com>
2021-06-16 15:56:16 -0600

    added comments from snapcode

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
-const generatePage = require('./src/page-template.js'); //module.exports is reassigned to this variable (generatePage)
-const profileDataArgs = process.argv.slice(2, process.argv.length); //holds the user command-line arguements
+//module.exports is reassigned to this variable (generatePage)
+const generatePage = require('./src/page-template.js');
+//holds the user command-line arguements
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+//replace above with this one(assignment destructuring) //
+const [name, github] = profileDataArgs;
 
-const [name, github] = profileDataArgs; //replace above with this one(assignment destructuring) //
-
-
+//const pageHTML = generatePage(name, github);
+//fs.writeFile('./index.html', pageHTML, err => {
+//if (err) throw err;
 
 fs.writeFile('index.html', generatePage(name, github), err => { //
     if (err) throw err; //
