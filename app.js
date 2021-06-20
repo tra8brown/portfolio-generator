ac785b5d551ae0a42ea855ab169f90862dac55c5
TraBee <tra8brown@gmail.com>
2021-06-19 18:29:00 -0600

    added inquirer pkg

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const fs = require('fs');
+//const fs = require('fs');
+const inquirer = require('inquirer');
+//const generatePage = require('./src/page-template');
+
 //module.exports is reassigned to this variable (generatePage)
-const generatePage = require('./src/page-template.js');
 //holds the user command-line arguements
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//replace above with this one(assignment destructuring) //
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 //const pageHTML = generatePage(name, github);
+//replace above with this one(assignment destructuring) //
+//const [name, github] = profileDataArgs;
+
 //fs.writeFile('./index.html', pageHTML, err => {
 //if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => { //
-    if (err) throw err; //
+// fs.writeFile('index.html', generatePage(name, github), err => { //
+//     if (err) throw err; //
 
-    console.log('Portfolio complete! Check out index.html to see the output!') //
-});
+//    console.log('Portfolio complete! Check out index.html to see the output!') //
+//});
+
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'what is your name?'
+    }])
+    .then(answers => console.log(answers));
